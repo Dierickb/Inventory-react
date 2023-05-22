a0e737db074ca79046c280790628eb2a096e26bf
Dierick Brochero <71480347+Dierickb@users.noreply.github.com>
2023-05-21 20:52:39 -0500

    feat: button device change color accord date

--- src/Components/Detail/Device/Device.js
+++ src/Components/Detail/Device/Device.js
@@ -1,19 +1,33 @@
 import './deviceRow.scss'
 import {memo} from "react";
 import PropTypes from "prop-types";
+import {Button} from "../../layout";
 
 const Device = ({device, index, handleShowDevice}) => {
     const handleDevice = () => {
         handleShowDevice(device)
     }
 
+    const dateParts = device.pxeDate.split("/");
+    const dateObject = new Date(dateParts[2], dateParts[1] - 1, +dateParts[0]);
+    const dateNow = new Date()
+
+    const rest = dateNow.getTime() - dateObject.getTime()
+    const days = Math.round(rest/ (1000*60*60*24))
+
+    const backGround = () => {
+        if (days > 13) return "#621717"
+        if(days <= 13  && days > 5) return "#CCBC3A"
+        return "#088993FF"
+    }
+
   return (
       <tr>
           <td>{index}</td>
           <td>
-              <button onClick={handleDevice}>
+              <Button background={backGround} onClick={handleDevice}>
                   {device.serial}
-              </button>
+              </Button>
           </td>
           <td>{device.brand}</td>
           <td>{device.product}</td>
--- src/Components/layout/inputs/Button.js
+++ src/Components/layout/inputs/Button.js
@@ -2,13 +2,14 @@ import styled from 'styled-components'
 
 export const Button = styled.button`
   width: 100%;
-  height: ${ (props) => props.hg ? props.hg : '35px'};
+  height: ${ (props) => props.hg ? props.hg : '25px'};
   position: relative;
   padding: 2px 4px;
   color: ${ (props) => props.color ? props.color : 'black'};
-  background: ${ (props) => props.background ? props.background : '#08ADBF'};
+  background: ${ (props) => props.background ? props.background : '#088993'};
   border-radius: 2rem;
   cursor: pointer;
+  color: ${ (props) => !!props?.background && "antiquewhite" };
   text-decoration: none;
   font-weight: bold;
   font-size: initial;
