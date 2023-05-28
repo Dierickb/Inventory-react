d7445bec223e238acf2b8bee39eb405d9f7a4b82
Dierick Brochero <71480347+Dierickb@users.noreply.github.com>
2023-05-28 12:16:09 -0500

    feat: Device.js unit tested

---
+++ src/Components/Detail/Device/__test__/Device.test.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import {initialDevice} from "../../utils/initialDevice";
+import Device from "../Device";
+import {device1, device2} from "../mocks/Device";
+
+describe('renders Device initial device', () => {
+    it('renders Device initial device', () => {
+        render(
+            <table className='devices'>
+                <thead>
+                <tr className='titlesHead'>
+                    <th>Id</th>
+                    <th>Serial</th>
+                    <th>Fabricante</th>
+                    <th>Producto</th>
+                    <th>Modelo</th>
+                    <th>Ingresó</th>
+                    <th>Scotia</th>
+                    <th>PXE</th>
+                    <th>Imagen</th>
+                </tr>
+                </thead>
+                <tbody>
+                <Device device={initialDevice} index={1}  />
+                </tbody>
+            </table>
+        )
+        const linkElement = screen.getByText(/1/i);
+        expect(linkElement).toBeInTheDocument();
+    })
+
+    it('renders Device device1', () => {
+        render(
+            <table className='devices'>
+                <thead>
+                <tr className='titlesHead'>
+                    <th>Id</th>
+                    <th>Serial</th>
+                    <th>Fabricante</th>
+                    <th>Producto</th>
+                    <th>Modelo</th>
+                    <th>Ingresó</th>
+                    <th>Scotia</th>
+                    <th>PXE</th>
+                    <th>Imagen</th>
+                </tr>
+                </thead>
+                <tbody>
+                <Device device={device1} index={1}  />
+                </tbody>
+            </table>
+        );
+        const product = screen.getByText(/Laptop/i);
+        const brand = screen.getByText(/Lenovo/i);
+        const model = screen.getByText(/T14/i);
+        const serial = screen.getByText(/Serial1/i);
+        const entryDate = screen.getByText(/11\/1\/2022/i);
+        const image = screen.getByText(/Sin Imagen/i);
+        const business = screen.getByText(/GSG/i);
+
+        expect(product).toBeInTheDocument();
+        expect(brand).toBeInTheDocument();
+        expect(model).toBeInTheDocument();
+        expect(serial).toBeInTheDocument();
+        expect(entryDate).toBeInTheDocument();
+        expect(image).toBeInTheDocument();
+        expect(business).toBeInTheDocument();
+    })
+
+    it('renders Device device2', () => {
+        render(
+            <table className='devices'>
+                <thead>
+                <tr className='titlesHead'>
+                    <th>Id</th>
+                    <th>Serial</th>
+                    <th>Fabricante</th>
+                    <th>Producto</th>
+                    <th>Modelo</th>
+                    <th>Ingresó</th>
+                    <th>Scotia</th>
+                    <th>PXE</th>
+                    <th>Imagen</th>
+                </tr>
+                </thead>
+                <tbody>
+                <Device device={device2} index={2}  />
+                </tbody>
+            </table>
+        );
+        const index = screen.getByText(/2/i);
+        const product = screen.getByText(/Laptop/i);
+        const brand = screen.getByText(/Lenovo/i);
+        const model = screen.getByText(/T14/i);
+        const serial = screen.getByText(/Serial2/i);
+        const entryDate = screen.getByText(/12\/1\/2022/i);
+        const image = screen.getByText(/ScotiaTech/i);
+        const business = screen.getByText(/GBS/i);
+        const pxeDate = screen.getByText(/11\/7\/2022/i)
+
+        expect(product).toBeInTheDocument();
+        expect(brand).toBeInTheDocument();
+        expect(model).toBeInTheDocument();
+        expect(serial).toBeInTheDocument();
+        expect(entryDate).toBeInTheDocument();
+        expect(image).toBeInTheDocument();
+        expect(business).toBeInTheDocument();
+        expect(pxeDate).toBeInTheDocument();
+        expect(index).toBeInTheDocument()
+    })
+
+    it('render Device handleShow', () => {
+        const showHandle = () => {
+            console.log('showHandle: ', device2)
+        }
+        render(
+            <table className='devices'>
+                <thead>
+                <tr className='titlesHead'>
+                    <th>Id</th>
+                    <th>Serial</th>
+                    <th>Fabricante</th>
+                    <th>Producto</th>
+                    <th>Modelo</th>
+                    <th>Ingresó</th>
+                    <th>Scotia</th>
+                    <th>PXE</th>
+                    <th>Imagen</th>
+                </tr>
+                </thead>
+                <tbody>
+                <Device device={device2} index={1} handleShowDevice={showHandle} />
+                </tbody>
+            </table>
+        );
+
+    })
+})
+
+
+
---
+++ src/Components/Detail/Device/mocks/Device.js
@@ -0,0 +1,23 @@
+export const device1 = {
+    brand: "Lenovo",
+    product: "Laptop",
+    model: "T14",
+    internOperation: "",
+    serial: "Serial1",
+    entryDate: "11/1/2022",
+    pxeDate: "",
+    image: "Sin Imagen",
+    business: "GSG",
+}
+
+export const device2 = {
+    brand: "Lenovo",
+    product: "Laptop",
+    model: "T14",
+    internOperation: "IT Support",
+    serial: "Serial2",
+    entryDate: "12/1/2022",
+    pxeDate: "11/7/2022",
+    image: "ScotiaTech",
+    business: "GBS",
+}
