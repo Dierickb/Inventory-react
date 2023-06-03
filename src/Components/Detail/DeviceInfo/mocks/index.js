2060b93f5dd1ac97fcc029722a42b07f0ad73274
Dierick Brochero <71480347+Dierickb@users.noreply.github.com>
2023-06-03 18:41:02 -0500

    fix: add .mock extension

---
+++ src/Components/Detail/DeviceInfo/mocks/index.js
@@ -0,0 +1,5 @@
+import {renderCorrectly} from "./BodyDeviceInfo.mock";
+
+export {
+    renderCorrectly
+}
---
+++ src/Components/Detail/mocks/index.js
@@ -0,0 +1,8 @@
+import { date, device1, device2, dateResult } from "./Device.mock"
+
+export {
+    date,
+    device1,
+    device2,
+    dateResult,
+}
