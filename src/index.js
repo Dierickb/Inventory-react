242430dfe1ef5035385c39a8a103f1391b79b853
Dierick Brochero <71480347+Dierickb@users.noreply.github.com>
2023-06-18 18:56:08 -0500

    feat: add react tostify and initialize

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './utils/firebase';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer/>
   </React.StrictMode>
 );
 
