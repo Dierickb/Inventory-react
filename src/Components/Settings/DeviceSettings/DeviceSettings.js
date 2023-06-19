5257ccad1bc87ed4a5285d3d00cd03cc5521aae5
Dierick Brochero <71480347+Dierickb@users.noreply.github.com>
2023-06-19 12:05:22 -0500

    feat: DeviceSetting BootCenterDevices

--- src/Components/Settings/DeviceSettings/DeviceSettings.js
+++ src/Components/Settings/DeviceSettings/DeviceSettings.js
@@ -1,33 +1,39 @@
+import {memo} from "react";
+
 import CardsContainer from "../../Card/CardsContainer";
 import FormSetDevice from "../../Forms/FormDevice/FormSetDevice";
 import CardContainer from "../../Card/CardContainer";
-import {Input} from "../../layout";
+import FormDevices from "../../Forms/FormDevice/FormDevices/FormDevices";
 import FormGetDevice from "../../Forms/FormDevice/FormGetDevice/FormGetDevice";
-import {memo} from "react";
+
+import {Input} from "../../layout";
 import {deviceSettingsKeysValues} from "../../../utils/utilities";
 
 const DeviceSettings = ({children}) => {
 
   return (
       <CardsContainer title="Device Settings">
-          <CardContainer title="Registrar equipo"
-                         Children={FormSetDevice} hasChild={true}
-                         keyValue={deviceSettingsKeysValues.REGISTER_DEVICE}
-          />
+            <CardContainer  title="Registrar equipo"
+                            Children={FormSetDevice} hasChild={true}
+                            keyValue={deviceSettingsKeysValues.REGISTER_DEVICE} />
 
-          <CardContainer title="Editar equipo"
-                         keyValue={deviceSettingsKeysValues.EDIT_DEVICE}
-                         info={
-                             <Input title="Serial" type="text" placeholder="Serial" name="Serial" wd="auto"/>
-                         }
-                         Children={FormGetDevice} hasChild={true} hasTextInput={true}/>
+            <CardContainer title="Editar equipo"
+                            keyValue={deviceSettingsKeysValues.EDIT_DEVICE}
+                            info={
+                                <Input title="Serial" type="text" placeholder="Serial" name="Serial" wd="auto"/>
+                            }
+                            Children={FormGetDevice} hasChild={true} hasTextInput={true} />
 
-          <CardContainer title="Eliminar equipo"
-                         keyValue={deviceSettingsKeysValues.DELETE_DEVICE}
-                         info={
-                             <Input title="Serial" type="text" placeholder="Serial" name="Serial" wd="auto"/>
-                         }
-                         Children={FormGetDevice} hasChild={true} hasTextInput={true}/>
+            <CardContainer title="Eliminar equipo"
+                            keyValue={deviceSettingsKeysValues.DELETE_DEVICE}
+                            info={
+                                <Input title="Serial" type="text" placeholder="Serial" name="Serial" wd="auto"/>
+                            }
+                            Children={FormGetDevice} hasChild={true} hasTextInput={true} />
+
+            <CardContainer title="Boot Center Devices"
+                                    Children={FormDevices} hasChild={true} />
+
           {!!children && children}
       </CardsContainer>
   )
