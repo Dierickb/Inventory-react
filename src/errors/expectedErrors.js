786c0947c99dc2d9ccb88e5431f4bd559d7b5cc2
Dierick Brochero <71480347+Dierickb@users.noreply.github.com>
2023-06-24 16:59:53 -0500

    feat: ipc expected errors and ipcMessages

--- src/errors/expectedErrors.js
+++ src/errors/expectedErrors.js
@@ -1,4 +1,10 @@
 export const ipcDeviceAPIExpectedError = {
     setDeviceAPI_DEVICE_ALREADY_EXIST: "Error invoking remote method 'SET_DEVICE': MessageValidation: DEVICE ALREADY EXIST",
     updateAPI_DEVICE_DOES_NOT_EXIST: "Error invoking remote method 'UPDATE_DEVICE': MessageValidation: DEVICE DOES NOT EXIST",
+    finDeviceAPI_DEVICE_NOT_FOUND: "Error invoking remote method 'FIND_DEVICE': MessageValidation: DEVICE NOT FOUND",
+    finDeviceByScotiaId_DEVICE_NOT_FOUND: "Error invoking remote method 'FIND_DEVICE_BY_SCOTIAID': MessageValidation: DEVICE NOT FOUND",
+    deleteDevice_DEVICE_NOT_FOUND: "Error invoking remote method 'DELETE_DEVICE': MessageValidation: DEVICE NOT FOUND",
+    findByImage_DEVICE_NOT_FOUND: "Error invoking remote method 'FIND_DEVICE_BY_IMAGE': MessageValidation: DEVICE NOT FOUND",
+    findByBusiness_DEVICE_NOT_FOUND: "Error invoking remote method 'FIND_DEVICE_BY_BUSINESS': MessageValidation: DEVICE NOT FOUND",
+    findByBusinessAndImage_DEVICE_NOT_FOUND: "Error invoking remote method 'FIND_DEVICE_BY_IMAGE_AND_BUSINESS': MessageValidation: DEVICE NOT FOUND",
 }
