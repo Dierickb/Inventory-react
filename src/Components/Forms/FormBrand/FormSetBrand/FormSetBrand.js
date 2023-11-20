16d6d2845a97fad47a05f4f9fd48406659522b4f
Dierick Brochero <71480347+Dierickb@users.noreply.github.com>
2023-11-20 11:48:44 -0500

    clean: remove unnecessary inputs

--- src/Components/Forms/FormBrand/FormSetBrand/FormSetBrand.js
+++ src/Components/Forms/FormBrand/FormSetBrand/FormSetBrand.js
@@ -2,8 +2,6 @@ import {InputsContainer, FormRegisters, Button, Input} from "../../../layout";
 import {DropDown} from "../../../layout/inputs";
 import {useHandleBrand} from "./hooks"
 
-import FormBrandDropDown from "../common/FormBrandDropDown";
-
 const FormSetBrand = () => {
 
     const {brands, handleBrand} = useHandleBrand()    
@@ -14,25 +12,9 @@ const FormSetBrand = () => {
                 <Input title="Fabricante" type="text" placeholder="Fabricante" name="newBrand"/>
             </InputsContainer>
 
-            <InputsContainer pd='2px px'>
-                <FormBrandDropDown key={brands?.length} showNewFirst={true} />
-            </InputsContainer> 
-
             <InputsContainer pd='2px 5px'>
-                <Input title="Producto" type="text" placeholder="Producto" name="newProduct"/>
-            </InputsContainer>
-
-            <InputsContainer pd='2px 5px'>
-                hola
-            </InputsContainer>  
-
-            <InputsContainer pd='2px 5px'>
-                <Input title="Modelo" type="text" placeholder="Modelo" name="newModel"/>
-            </InputsContainer>
-
-            <InputsContainer pd='2px 5px'>
-                hola
-            </InputsContainer>            
+                <Input title="Producto" type="text" placeholder="Producto/s" name="newProduct"/>
+            </InputsContainer>       
 
             <InputsContainer className="begin" gridColEnd="3">
                 <Button>Submit</Button>
