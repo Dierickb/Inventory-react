057f9980c151c8128f480d21ecf3c63ca94c22ae
Dierickb <dierickbro@gmail.com>
2025-01-05 23:30:32 -0500

    remove firebae components

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,3 @@
-export {authAPI} from './authAPI'
-export {deviceAPI} from './deviceAPI'
-export {brandAPI} from './brandAPI'
 export {ipcBrandAPI} from "./ipcBrandAPI"
 export {ipcOperations} from "./ipcOperations"
 export {ipcDeviceAPI} from "./ipcDeviceAPI"
